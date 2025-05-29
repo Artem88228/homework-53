5d66f62d98cb69d9784eb8f0d90afbd3c1f97fee
Artem <artembargatin@gmail.com>
2025-05-29 14:58:39 +0600

    State initialized

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,12 @@
 import './App.css'
+import {useState} from "react";
 
 const App = () => {
+    const [tasks, setTasks] = useState([
+        {task: 'Buy milk', id: '1'},
+        {task: 'Walk with dog', id: '2'},
+        {task: 'Do homework', id: '3'}
+    ])
 
   return (
     <>
